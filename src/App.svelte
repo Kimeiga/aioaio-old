0cb1d25f2dfd54f92c79c168b57550d197f1eb62
Hakan Alpay <hak7alp@gmail.com>
2023-09-01 00:07:53 -0700

    s

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+
+  import { username } from "./store.js";
+  username.useLocalStorage();
+</script>
+
+<Router {routes} />
+
+<style global>
+  @import "../assets/global.css";
+  body {
+    margin: 0;
+  }
+
+  html {
+    font-family: sans-serif;
+  }
+</style>
